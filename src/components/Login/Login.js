251beea5be9b981aeb6a14704483f57d00b716cd
ethanlolomg <ethanlolomg@gmail.com>
2022-10-16 09:32:39 -0700

    Switched login from class to functional component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -23,124 +23,127 @@ const socialData = [
     icon: "fab fa-google-plus-g",
   },
 ];
+// const[initData, setInitData] = useState({});
+// const[data, setData] = useState([]);
 
-class Login extends Component {
-  state = {
-    initData: {},
-    data: [],
-  };
 
-  componentDidMount() {
-    this.setState({
-      initData: initData,
-      data: socialData,
-    });
-  }
-  render() {
-    const toast = useToast()
-    return (
-      <section className="author-area">
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-12 col-md-8 col-lg-7">
-              {/* Intro */}
-              <div className="intro text-center">
-                <span>{this.state.initData.pre_heading}</span>
-                <h3 className="mt-3 mb-0">{this.state.initData.heading}</h3>
-                <p>{this.state.initData.content}</p>
-              </div>
-              {/* Item Form */}
-              <form className="item-form card no-hover" action="http://localhost:8080/login">
-                <div className="row">
-                  <div className="col-12">
-                    <div className="form-group mt-3">
+const Login = () => {
+  // state = {
+  //   initData: {},
+  //   data: [],
+  // };
+
+  // componentDidMount() {
+  //   this.setState({
+  //     initData: initData,
+  //     data: socialData,
+  //   });
+  // }
+  // render() {
+  const toast = useToast()
+  return (
+    <section className="author-area">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-12 col-md-8 col-lg-7">
+            {/* Intro */}
+            <div className="intro text-center">
+              <span>{initData.pre_heading}</span>
+              <h3 className="mt-3 mb-0">{initData.heading}</h3>
+              <p>{initData.content}</p>
+            </div>
+            {/* Item Form */}
+            <form className="item-form card no-hover" action="http://localhost:8080/login">
+              <div className="row">
+                <div className="col-12">
+                  <div className="form-group mt-3">
+                    <input
+                      type="email"
+                      className="form-control"
+                      name="email"
+                      placeholder="Enter your Email"
+                      required="required"
+                    />
+                  </div>
+                </div>
+                <div className="col-12">
+                  <div className="form-group mt-3">
+                    <input
+                      type="password"
+                      className="form-control"
+                      name="password"
+                      placeholder="Enter your Password"
+                      required="required"
+                    />
+                  </div>
+                </div>
+                <div className="col-12">
+                  <div className="form-group mt-3">
+                    <div className="form-check form-check-inline">
                       <input
-                        type="email"
-                        className="form-control"
-                        name="email"
-                        placeholder="Enter your Email"
-                        required="required"
+                        className="form-check-input"
+                        type="radio"
+                        name="inlineRadioOptions"
+                        id="inlineRadio1"
+                        defaultValue="option1"
+                        defaultChecked
                       />
-                    </div>
-                  </div>
-                  <div className="col-12">
-                    <div className="form-group mt-3">
-                      <input
-                        type="password"
-                        className="form-control"
-                        name="password"
-                        placeholder="Enter your Password"
-                        required="required"
-                      />
-                    </div>
-                  </div>
-                  <div className="col-12">
-                    <div className="form-group mt-3">
-                      <div className="form-check form-check-inline">
-                        <input
-                          className="form-check-input"
-                          type="radio"
-                          name="inlineRadioOptions"
-                          id="inlineRadio1"
-                          defaultValue="option1"
-                          defaultChecked
-                        />
-                        <label
-                          className="form-check-label"
-                          htmlFor="inlineRadio1"
-                        >
-                          Remember Me
-                        </label>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-12">
-                    <button className="btn w-100 mt-3 mt-sm-4" type="submit" onClick={()=>{
-                      toast({
-                        title: "Login Success",
-                        description: "Welcome",
-                        status: "success",
-                        duration: 9000,
-                        isClosable: true,
-                      });
-                    }}>
-                      Sign In
-                    </button>
-                  </div>
-                  <div className="col-12">
-                    <span className="d-block text-center mt-4">
-                      Do not have an account? <a href="/signup">Signup</a>
-                    </span>
-                  </div>
-                  <div className="col-12">
-                    <hr />
-                    <div className="other-option">
-                      <span className="d-block text-center mb-4">Or</span>
-                      {/* Social Icons */}
-                      <div className="social-icons d-flex justify-content-center">
-                        {this.state.data.map((item, idx) => {
-                          return (
-                            <a
-                              key={`lsd_${idx}`}
-                              className={item.link}
-                              href="#"
-                            >
-                              <i className={item.icon} />
-                              <i className={item.icon} />
-                            </a>
-                          );
-                        })}
-                      </div>
+                      <label
+                        className="form-check-label"
+                        htmlFor="inlineRadio1"
+                      >
+                        Remember Me
+                      </label>
                     </div>
                   </div>
                 </div>
-              </form>
-            </div>
+                <div className="col-12">
+                  <button className="btn w-100 mt-3 mt-sm-4" type="submit" onClick={()=>{
+                    toast({
+                      title: "Login Success",
+                      description: "Welcome",
+                      status: "success",
+                      duration: 9000,
+                      isClosable: true,
+                    });
+                  }}>
+                    Sign In
+                  </button>
+                </div>
+                <div className="col-12">
+                  <span className="d-block text-center mt-4">
+                    Do not have an account? <a href="/signup">Signup</a>
+                  </span>
+                </div>
+                <div className="col-12">
+                  <hr />
+                  <div className="other-option">
+                    <span className="d-block text-center mb-4">Or</span>
+                    {/* Social Icons */}
+                    <div className="social-icons d-flex justify-content-center">
+                      {socialData.map((item, idx) => {
+                        return (
+                          <a
+                            key={`lsd_${idx}`}
+                            className={item.link}
+                            href="#"
+                          >
+                            <i className={item.icon} />
+                            <i className={item.icon} />
+                          </a>
+                        );
+                      })}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
-      </section>
-    );
-  }
+      </div>
+    </section>
+  );
+  // }
 }
 
 export default Login;
